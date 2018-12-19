ee9d59cb04647df36da79932a439b1d4a1cb6ec3
lchancri <lchancri@student.42.fr>
2018-12-19 20:10:10 +0100

    Fixed accuracy, added mirroring, need more data augmentation

--- get_image.py
+++ get_image.py
@@ -1,16 +1,42 @@
 import numpy as np
 import h5py
-
+import matplotlib.pyplot as plt
 class data:
 
     def __init__(self, train_image, test_image, split):
         self.train_data, self.train_label = self.unpack_image(train_image, 0)
+        label = self.train_label
+        data = self.train_data
+        self.data_augmentation(data, label)
+        np.random.seed(7)
+        np.random.shuffle(self.train_data)
+        np.random.seed(7)
+        np.random.shuffle(self.train_label)
         self.test_data, self.test_label = self.unpack_image(test_image, 1)
-        #train_length = int(np.shape(train_data)[0] * split)
-        #self.train_data = train_data[:train_length]
-        #self.train_label = train_label[:train_length]
-        #self.validation_data = train_data[train_length:]
-        #self.validation_label = train_label[train_length:]
+
+    def reverse(self, data, i, line, col, chanel):
+        tmp2 = [0] * line
+        for j in range(0, line):
+            tmp3 = [0] * col
+            for k in range(0, col):
+                tmp4 = [0] * chanel
+                for l in range(0, chanel):
+                    x = data[i][j][col - 1 - k][l]
+                    tmp4[l] = x
+                tmp3[k] = tmp4
+            tmp2[j] = tmp3
+        return (tmp2)
+
+    def data_augmentation(self, data, label):
+        # mirroring, rotation
+        train_data2 = []
+        nb_image, line, col, chanel = np.shape(data)
+        tmp = [0] * nb_image
+        for i in range(0, nb_image):
+            tmp[i] = self.reverse(data, i, line, col, chanel)
+        tmp = np.reshape(tmp, (nb_image, line, col, chanel))
+        self.train_data = np.concatenate((self.train_data, tmp), axis=0)
+        self.train_label = np.concatenate((self.train_label, label), axis=0)
 
     def unpack_image(self, path, train_or_test):
         data = h5py.File(path)
--- main.py
+++ main.py
@@ -25,22 +25,31 @@ def plot(train_loss, test_loss, train_accuracy, test_accuracy):
     plt.show()
 
 def my_weight_and_bias(nb_class, nb_chanel, batch_size):
+    w = {'conv1': tf.Variable(tf.random_normal([3,3,nb_chanel,32])),
+         'conv2': tf.Variable(tf.random_normal([3,3,32,64])),
+         'conv3': tf.Variable(tf.random_normal([3,3,64,128])),
+         'fc1': tf.Variable(tf.random_normal([8*8*128,128])),
+         'output': tf.Variable(tf.random_normal([128, nb_class]))}
+    b = {'conv1': tf.Variable(tf.random_normal([32])),
+         'conv2': tf.Variable(tf.random_normal([64])),
+         'conv3': tf.Variable(tf.random_normal([128])),
+         'fc1': tf.Variable(tf.random_normal([128])),
+         'output': tf.Variable(tf.random_normal([nb_class]))}
+    return (w, b)
+
+def my_weight_and_bias2(nb_class, nb_chanel, batch_size):
     w = {'conv1': tf.Variable(tf.random_normal([3,3,nb_chanel,32], seed=3)),
-         'conv2': tf.Variable(tf.random_normal([3,3,32,64], seed=1)),
-         'conv3': tf.Variable(tf.random_normal([3,3,64,128], seed=4)),
-         'fc1': tf.Variable(tf.random_normal([8*8*128,128], seed=5)),
-         'output': tf.Variable(tf.random_normal([128, nb_class], seed=4))}
+         'fc1': tf.Variable(tf.random_normal([32*32*32,32], seed=5)),
+         'output': tf.Variable(tf.random_normal([32, nb_class], seed=4))}
     b = {'conv1': tf.Variable(tf.random_normal([32], seed=3)),
-         'conv2': tf.Variable(tf.random_normal([64], seed=2)),
-         'conv3': tf.Variable(tf.random_normal([128], seed=2)),
-         'fc1': tf.Variable(tf.random_normal([128], seed=9)),
+         'fc1': tf.Variable(tf.random_normal([32], seed=9)),
          'output': tf.Variable(tf.random_normal([nb_class], seed=5))}
     return (w, b)
 
 def main():
-    alpha = 0.4
-    num_epoch = 20
-    nb_class = 2
+    alpha = 0.05
+    num_epoch = 200
+    nb_class = np.shape(data.train_label)[1]
     batch_size = 60
     nb_image, line, col, nb_chanel = np.shape(data.train_data)
     data.train_data = np.reshape(data.train_data, [nb_image, line, col, nb_chanel])
@@ -52,8 +61,7 @@ def main():
     weight, bias = my_weight_and_bias(nb_class, nb_chanel, batch_size) 
 
     pred = mod.MyModel(x, weight, bias)
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=pred,
-        labels=y))
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=pred, labels=y))
     optimizer = tf.train.AdamOptimizer(learning_rate=alpha).minimize(cost)
     correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
@@ -71,8 +79,9 @@ def main():
                     len(data.train_data))]
                 batch_y = data.train_label[i*batch_size:min((i+1)*batch_size,
                     len(data.train_label))]
+                #print(sess.run(pred, feed_dict={x: data.test_data, y: data.test_label}))
                 sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
-            TmpLoss, TmpAcc = sess.run([cost, accuracy], feed_dict={x: batch_x, y: batch_y})
+            TmpLoss, TmpAcc = sess.run([cost, accuracy], feed_dict={x: data.train_data, y: data.train_label})
             TmpTestLoss, TmpTestAcc = sess.run([cost, accuracy],
                     feed_dict={x: data.test_data, y: data.test_label})
             print("Epoch :", e, ", Loss :", TmpLoss, "test lost :", TmpTestLoss, ", Train acc :",
